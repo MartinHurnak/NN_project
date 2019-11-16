09c37fc281afc864a6c1aa17fcfe6cc502c44efa
MartinHurnak <hurnak.m@gmail.com>
2019-11-16 20:21:13 +0100

    added rescale to data generator

--- src/models/DataGen.py
+++ src/models/DataGen.py
@@ -1,11 +1,12 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from config import BATCH_SIZE
 def datagen_flow(data):
     datagen = ImageDataGenerator()
     return datagen.flow_from_dataframe(data,
                                 directory='../data/raw/VOC2012/JPEGImages',
                                 x_col='filename',
-                                y_col=['class', 'is_object', 'bb_coords', 'bb_sizes'],
+                                y_col=['is_object', 'bb_coords', 'bb_sizes', 'class'],
                                 target_size=(256, 256),
+                                rescale = 1.0/255,
                                 class_mode='multi_output',
-                                batch_size=32)
+                                batch_size=BATCH_SIZE)
