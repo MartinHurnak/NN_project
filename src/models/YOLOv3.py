fa6920ec56d856b8f0eb81ffbb1a31d0451682c5
MartinHurnak <hurnak.m@gmail.com>
2019-11-16 20:21:54 +0100

    changed model creation to Keras functiona Api

--- src/models/YOLOv3.py
+++ src/models/YOLOv3.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, add, Activation, GlobalAveragePooling2D, Dense, concatenate, RepeatVector, \
     LSTM, Bidirectional
-from config import SEQUENCE_LENGTH, CONV_ACTIVATION
+from config import SEQUENCE_LENGTH, CONV_ACTIVATION, CONV_BASE_SIZE, LSTM_SIZE
 
 
 class YoloLayer(keras.layers.Layer):
@@ -19,55 +19,89 @@ class YoloLayer(keras.layers.Layer):
         l = self.activation(l)
         return l
 
+#
+# class OutputLayer(keras.layers.Layer):
+#     def __init__(self, num_classes):
+#         super(OutputLayer, self).__init__()
+#         self.lstm = Bidirectional(LSTM(1024, return_sequences=True))
+#
+#         self.bb_coord = Dense(2, activation='sigmoid', name='bb_coord')
+#         self.bb_size = Dense(2, activation='sigmoid', name='bb_size')
+#         self.has_object = Dense(1, activation='sigmoid', name='is_object_output')
+#         self.classes = Dense(num_classes, activation='softmax', name='class_output')
+#
+#     def call(self, x):
+#         x = self.lstm(x)
+#         bb_coord = self.bb_coord(x)
+#         bb_size = self.bb_size(x)
+#         has_object = self.has_object(x)
+#         classes = self.classes(x)
+#
+#         return has_object, bb_coord, bb_size, classes
+#
+#
+# class YOLOv3(keras.Model):
+#     '''
+#     https://pjreddie.com/media/files/papers/YOLOv3.pdf
+#     '''
+#
+#     def __init__(self, num_classes):
+#         super(YOLOv3, self).__init__()
+#         self.model_layers = [
+#             Conv2D(32, (3, 3), padding='same', activation=CONV_ACTIVATION),
+#             Conv2D(64, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
+#             YoloLayer(32, 64, activation=CONV_ACTIVATION),
+#             Conv2D(128, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
+#         ]
+#         self.model_layers += [YoloLayer(64, 128, activation=CONV_ACTIVATION)] * 2
+#         self.model_layers.append(Conv2D(256, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+#         self.model_layers += [YoloLayer(128, 256, activation=CONV_ACTIVATION)]  # *8
+#         self.model_layers.append(Conv2D(512, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+#         self.model_layers += [YoloLayer(256, 512, activation=CONV_ACTIVATION)]  # *8
+#         self.model_layers.append(Conv2D(1024, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+#         self.model_layers += [YoloLayer(512, 1024, activation=CONV_ACTIVATION)]  # *4
+#         self.model_layers += [
+#             GlobalAveragePooling2D(),
+#             # Dense(1000),
+#             RepeatVector(SEQUENCE_LENGTH),
+#             OutputLayer(num_classes)
+#         ]
+#
+#     def call(self, x):
+#         for layer in self.model_layers:
+#             x = layer(x)
+#         return x
 
-class OutputLayer(keras.layers.Layer):
-    def __init__(self, num_classes):
-        super(OutputLayer, self).__init__()
-        self.lstm = Bidirectional(LSTM(1024, return_sequences=True))
+def create_model(num_classes):
+    input = keras.layers.Input(shape=(256,256,3,))
+    model_layers = [
+        Conv2D(CONV_BASE_SIZE, (3, 3), padding='same', activation=CONV_ACTIVATION),
+        Conv2D(2*CONV_BASE_SIZE, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
+        YoloLayer(CONV_BASE_SIZE, 2*CONV_BASE_SIZE, activation=CONV_ACTIVATION),
+        Conv2D(4*CONV_BASE_SIZE, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
+    ]
+    model_layers += [YoloLayer(2*CONV_BASE_SIZE, 4*CONV_BASE_SIZE, activation=CONV_ACTIVATION)] * 2
+    model_layers.append(Conv2D(8*CONV_BASE_SIZE, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+    model_layers += [YoloLayer(2*CONV_BASE_SIZE, 8*CONV_BASE_SIZE, activation=CONV_ACTIVATION)]   *8
+    model_layers.append(Conv2D(16*CONV_BASE_SIZE, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+    model_layers += [YoloLayer(8*CONV_BASE_SIZE, 16*CONV_BASE_SIZE, activation=CONV_ACTIVATION)]   *8
+    model_layers.append(Conv2D(32*CONV_BASE_SIZE, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
+    model_layers += [YoloLayer(16*CONV_BASE_SIZE, 32*CONV_BASE_SIZE, activation=CONV_ACTIVATION)]   *4
+    model_layers += [
+        GlobalAveragePooling2D(),
+        # Dense(1000),
+        RepeatVector(SEQUENCE_LENGTH),
+        Bidirectional(LSTM(LSTM_SIZE, return_sequences=True))
+    ]
 
-        self.bb_coord = Dense(2, activation='sigmoid', name='bb_coord')
-        self.bb_size = Dense(2, activation='sigmoid', name='bb_size')
-        self.has_object = Dense(1, activation='sigmoid', name='is_object_output')
-        self.classes = Dense(num_classes, activation='softmax', name='class_output')
+    has_object_true = keras.layers.Input(shape=(SEQUENCE_LENGTH,1,))
+   # outputs = OutputLayer(num_classes)
+    x = input
+    for layer in model_layers:
+        x = layer(x)
+    bb_coord = Dense(2, activation='sigmoid', name='bb_coord')(x)
+    bb_size = Dense(2, activation='sigmoid', name='bb_size')(x)
+    has_object = Dense(1, activation='sigmoid', name='is_object_output')(x)
+    classes = Dense(num_classes, activation='softmax', name='class_output')(x)
 
-    def call(self, x):
-        x = self.lstm(x)
-        bb_coord = self.bb_coord(x)
-        bb_size = self.bb_size(x)
-        has_object = self.has_object(x)
-        classes = self.classes(x)
-
-        return [classes, has_object, bb_coord, bb_size]
-
-
-class YOLOv3(keras.Model):
-    '''
-    https://pjreddie.com/media/files/papers/YOLOv3.pdf
-    '''
-
-    def __init__(self, num_classes):
-        super(YOLOv3, self).__init__()
-        self.model_layers = [
-            Conv2D(32, (3, 3), padding='same', activation=CONV_ACTIVATION),
-            Conv2D(64, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
-            YoloLayer(32, 64, activation=CONV_ACTIVATION),
-            Conv2D(128, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION),
-        ]
-        self.model_layers += [YoloLayer(64, 128, activation=CONV_ACTIVATION)] * 2
-        self.model_layers.append(Conv2D(256, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
-        self.model_layers += [YoloLayer(128, 256, activation=CONV_ACTIVATION)]  # *8
-        self.model_layers.append(Conv2D(512, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
-        self.model_layers += [YoloLayer(256, 512, activation=CONV_ACTIVATION)]  # *8
-        self.model_layers.append(Conv2D(1024, (3, 3), strides=2, padding='same', activation=CONV_ACTIVATION))
-        self.model_layers += [YoloLayer(512, 1024, activation=CONV_ACTIVATION)]  # *4
-        self.model_layers += [
-            GlobalAveragePooling2D(),
-            # Dense(1000),
-            RepeatVector(SEQUENCE_LENGTH),
-            OutputLayer(num_classes)
-        ]
-
-    def call(self, x):
-        for layer in self.model_layers:
-            x = layer(x)
-        return x
+    return keras.Model(inputs=input, outputs=[has_object, bb_coord, bb_size, classes]), has_object_true
