8dbd9944da30e2d4d0724d09a45e179059eba94f
MartinHurnak <hurnak.m@gmail.com>
2019-11-17 20:46:54 +0100

    Changed BB coordinates to <0,1> interval

--- src/helpers.py
+++ src/helpers.py
@@ -4,10 +4,10 @@ def get_center_size(xmin, ymin, xmax, ymax, img_width, img_height):
     if ymin > ymax:
         raise AttributeError('{} > {}'.format(ymin, ymax))
 
-    xmin = 2 * xmin / img_width - 1
-    ymin = 2 * ymin / img_height - 1
-    xmax = 2 * xmax / img_width - 1
-    ymax = 2 * ymax / img_height - 1
+    xmin = xmin / img_width
+    ymin = ymin / img_height
+    xmax = xmax / img_width
+    ymax = ymax / img_height
 
     w = abs(xmax - xmin)
     h = abs(ymax - ymin)
@@ -15,21 +15,19 @@ def get_center_size(xmin, ymin, xmax, ymax, img_width, img_height):
     x = xmin + w / 2
     y = ymin + h / 2
 
-    return x, y, w / 2, h / 2
+    return x, y, w, h
 
 
 def get_bb_min_max(x, y, w, h, img_width, img_height):
-    w *= 2
-    h *= 2
 
     xmin = x - w / 2
     ymin = y - h / 2
     xmax = x + w / 2
     ymax = y + h / 2
 
-    xmin = round((xmin + 1) * img_width / 2)
-    ymin = round((ymin + 1) * img_height / 2)
-    xmax = round((xmax + 1) * img_width / 2)
-    ymax = round((ymax + 1) * img_height / 2)
+    xmin = round(xmin * img_width )
+    ymin = round(ymin * img_height)
+    xmax = round(xmax * img_width)
+    ymax = round(ymax * img_height)
 
     return xmin, ymin, xmax, ymax
--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -3,20 +3,20 @@ from src.helpers import get_center_size, get_bb_min_max
 
 def test_get_center_sizes():
     x, y, w, h = get_center_size(0, 0, 500, 500, 500, 500)
-    assert x == 0
-    assert y == 0
+    assert x == 0.5
+    assert y == 0.5
     assert w == 1
     assert h == 1
 
     x, y, w, h = get_center_size(0, 0, 250, 250, 500, 500)
-    assert x == -0.5
-    assert y == -0.5
+    assert x == 0.25
+    assert y == 0.25
     assert w == 0.5
     assert h == 0.5
 
     x, y, w, h = get_center_size(125, 125, 375, 375, 500, 500)
-    assert x == 0
-    assert y == 0
+    assert x == 0.5
+    assert y == 0.5
     assert w == 0.5
     assert h == 0.5
 
@@ -25,21 +25,21 @@ def test_get_center_sizes():
             for k in range(i,10):
                 for l in range(j,10):
                     x, y, w, h = get_center_size(i, j, k, l, 9, 9)
-                    assert (x >= -1) and (x <= 1)
-                    assert (y >= -1) and (y <= 1)
+                    assert (x >= 0) and (x <= 1)
+                    assert (y >= 0) and (y <= 1)
                     assert (w >= 0) and (w <= 1)
                     assert (h >= 0) and (h <= 1)
 
 
 
 def test_get_bb_min_max():
-    xmin, ymin, xmax, ymax = get_bb_min_max(-1, -1, 1, 1, 500, 500)
+    xmin, ymin, xmax, ymax = get_bb_min_max(0, 0, 1, 1, 500, 500)
     assert xmin == 0
     assert xmax == 500
     assert ymin == 0
     assert ymax == 500
 
-    xmin, ymin, xmax, ymax = get_bb_min_max(-0.5, -0.5, 0.5, 0.5, 500, 500)
+    xmin, ymin, xmax, ymax = get_bb_min_max(0.5, 0.5, 0.5, 0.5, 500, 500)
     assert xmin == 125
     assert xmax == 125
     assert ymin == 375
