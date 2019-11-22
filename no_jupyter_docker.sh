60130eb23cfa3e4816355080b78d57aa905e85e0
MartinHurnak <hurnak.m@gmail.com>
2019-11-22 22:34:45 +0100

    Repair docker run command

--- no_jupyter_docker.sh
+++ no_jupyter_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --gpus all -p 8888:8888 -p 6006:6006 -v ${pwd}:/NN_project  -it --entrypoint /bin/bash mataur/nsiete_fiit
+sudo docker run --gpus all -p 8888:8888 -p 6006:6006 -v $(pwd):/NN_project  -it --entrypoint /bin/bash mataur/nsiete_fiit
