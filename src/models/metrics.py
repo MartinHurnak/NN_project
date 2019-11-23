415e7c51bc6277c134c1d1e66aa082af39b2ac32
MartinHurnak <hurnak.m@gmail.com>
2019-11-23 15:13:29 +0100

    Fix metrics

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -6,14 +6,14 @@ import numpy as np
 def recall(y_true, y_pred, threshold=0.5):
     tp = true_positives(y_true, y_pred, threshold)
     fn = false_negative(y_true, y_pred, threshold)
-    return K.clip(tp / (tp + fn), 0.0, 1.0)
+    return tp / (tp + fn)
 
 
 def precision(y_true, y_pred, threshold=0.5):
     tp = true_positives(y_true, y_pred, threshold)
     p = positives(y_true, y_pred, threshold)
-    precision = tp / p
-    return K.clip(precision, 0.0, 1.0)
+    zero_positives = K.cast(K.equal(p, K.zeros_like(p)),K.floatx())
+    return K.maximum(tp / (p+K.epsilon()), zero_positives)
 
 
 def positives(y_true, y_pred, threshold=0.5):
@@ -44,15 +44,12 @@ def true_positives(y_true, y_pred, threshold=0.5):
     pred_box_h = y_pred[..., 3]
     pred_box_conf = y_pred[..., 4]
 
-    #for i in range(16):
     iou = intersection_over_union(GRID_SIZE, pred_box_x, pred_box_y, pred_box_w, pred_box_h,
                                   true_box_x, true_box_y,
                                   true_box_w,true_box_h)
     tp = K.cast(K.greater(iou, 0.5 * K.ones_like(iou)), K.floatx()) * (
         K.cast(K.greater(pred_box_conf, threshold * K.ones_like(pred_box_conf)), K.floatx()))
-
-    tp = K.max(tp, axis=-1)
-    return K.sum(tp, axis=0)
+    return K.sum(tp, axis=-1)
 
 
 def false_negative(y_true, y_pred, threshold=0.5):
@@ -60,22 +57,7 @@ def false_negative(y_true, y_pred, threshold=0.5):
         y_pred = K.constant(y_pred)
     y_true = K.cast(y_true, y_pred.dtype)
 
-    true_box_x = y_true[..., 0]
-    true_box_y = y_true[..., 1]
-    true_box_w = y_true[..., 2]
-    true_box_h = y_true[..., 3]
     true_box_conf = y_true[...,4]
 
-    pred_box_x = y_pred[..., 0]
-    pred_box_y = y_pred[..., 1]
-    pred_box_w = y_pred[..., 2]
-    pred_box_h = y_pred[..., 3]
-    pred_box_conf = y_pred[..., 4]
-
-    iou = intersection_over_union(GRID_SIZE, pred_box_x, pred_box_y, pred_box_w, pred_box_h,
-                                  true_box_x, true_box_y,
-                                  true_box_w,true_box_h)
-    fn = K.cast(K.greater(iou, 0.5 * K.ones_like(iou)), K.floatx()) * (
-        K.cast(K.greater_equal(pred_box_conf, threshold * K.ones_like(pred_box_conf)), K.floatx()))
-    fn = K.cast(K.equal(K.max(fn, axis=0), K.zeros_like(K.max(fn, axis=0))), K.floatx()) * true_box_conf
-    return K.sum(fn, axis=-1)
+    fn = K.sum(true_box_conf, axis=-1) - true_positives(y_true, y_pred, threshold)
+    return fn
