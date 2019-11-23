cc1fc4635ba95e81a67637b40a1bdb41301a5075
MartinHurnak <hurnak.m@gmail.com>
2019-11-23 12:14:37 +0100

    Fixed imports, added command line arguments parsing

--- src/models/train.py
+++ src/models/train.py
@@ -1,7 +1,19 @@
+import os
+import sys
+os.chdir('/NN_project')
+sys.path.append("/NN_project")
+
 import pandas as pd
 from src.models.YOLOv3 import create_and_fit
 import config
+import argparse
+
+parser = argparse.ArgumentParser(prog='Train')
+parser.add_argument('--epochs', nargs='?', help='Number of epochs for fit', default=config.EPOCHS)
+parser.add_argument('--batch_size', nargs='?', help='Batch size', type=int, default=config.BATCH_SIZE)
+parser.add_argument('--val_split', nargs='?', help='Train/Validation split', type=int, default=config.VALIDATION_SPLIT)
+args = parser.parse_args()
 
 pickle_file = 'data/preprocessed/VOC2012/preprocessed_train.pkl'
 df = pd.read_pickle(pickle_file)
-model = create_and_fit(df, config.EPOCHS, config.BATCH_SIZE, verbose=1, val_split=config.VALIDATION_SPLIT)
+model = create_and_fit(df, epochs=args.epochs, batch_size=args.batch_size, verbose=1, val_split=args.val_split)
