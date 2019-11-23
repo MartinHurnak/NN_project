4c0e720ea9ddc92e0375d1f3133ebdb9ee2e810c
MartinHurnak <hurnak.m@gmail.com>
2019-11-23 11:19:21 +0100

    Use training data for training

--- src/models/train.py
+++ src/models/train.py
@@ -2,7 +2,6 @@ import pandas as pd
 from src.models.YOLOv3 import create_and_fit
 import config
 
-pickle_file = 'data/preprocessed/VOC2012/preprocessed.pkl'
+pickle_file = 'data/preprocessed/VOC2012/preprocessed_train.pkl'
 df = pd.read_pickle(pickle_file)
-df = df[df['has_person']].reset_index()
 model = create_and_fit(df, config.EPOCHS, config.BATCH_SIZE, verbose=1, val_split=config.VALIDATION_SPLIT)
