daf2aac683d20b9507ec74f6e420baf24861e3a1
MartinHurnak <hurnak.m@gmail.com>
2019-11-23 14:08:44 +0100

    Fixed epochs argument type

--- src/models/train.py
+++ src/models/train.py
@@ -10,7 +10,7 @@ import config
 import argparse
 
 parser = argparse.ArgumentParser(prog='Train')
-parser.add_argument('--epochs', nargs='?', help='Number of epochs for fit', default=config.EPOCHS)
+parser.add_argument('--epochs', nargs='?', help='Number of epochs for fit', type=int, default=config.EPOCHS)
 parser.add_argument('--batch_size', nargs='?', help='Batch size', type=int, default=config.BATCH_SIZE)
 parser.add_argument('--val_split', nargs='?', help='Train/Validation split', type=int, default=config.VALIDATION_SPLIT)
 parser.add_argument('--neg_box_coef', nargs='?', help='Constant for loss function', type=float,
