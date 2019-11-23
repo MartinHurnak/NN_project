360149e021c74ac05f0ade530b8452ebe4fbd2db
MartinHurnak <hurnak.m@gmail.com>
2019-11-23 10:07:45 +0100

    Fix training to whole dataset

--- src/models/train.py
+++ src/models/train.py
@@ -4,4 +4,4 @@ from src.models.YOLOv3 import create_and_fit
 pickle_file = 'data/preprocessed/VOC2012/preprocessed.pkl'
 df = pd.read_pickle(pickle_file)
 df = df[df['has_person']].reset_index()
-model = create_and_fit(df.head(32), 5, 64, verbose=1, val_split=0.1)
+model = create_and_fit(df, 30, 256, verbose=1, val_split=0.1)
